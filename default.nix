4871c0a7c0c8479ca2afa106349fe246447935af
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2022-04-03 22:16:31 +0200

    fully remove removed packages

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@ let
 in
 let
   python3AppPackages = pkgs.recurseIntoAttrs rec {
-    pyrouteros = throw "pyrouteros has been removed, because it is unmaintained"; # added 2022-04-03
     bundlewrap = pkgs.python3.pkgs.callPackage ./pkgs/development/python-modules/bundlewrap { lib = mylib; };
   };
 in
@@ -32,8 +31,6 @@ rec {
   encpipe-static = pkgs.callPackage ./pkgs/tools/security/encpipe { lib = mylib; static = true; dietlibc = dietlibc; };
 
   friidump = pkgs.callPackage ./pkgs/games/friidump { lib = mylib; };
-
-  git-smash = throw "git-smash has been removed, because it is unmaintained"; # added 2022-04-03
 
   falsisign = pkgs.callPackage pkgs/applications/misc/falsisign/default.nix { lib = mylib; };
   masterpdfeditor4 = pkgs.callPackage pkgs/applications/misc/masterpdfeditor/default.nix { lib = mylib; qtbase = pkgs.qt5.qtbase; qtsvg = pkgs.qt5.qtsvg; wrapQtAppsHook = pkgs.qt5.wrapQtAppsHook; };
